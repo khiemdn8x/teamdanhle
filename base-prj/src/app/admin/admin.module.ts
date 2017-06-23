5914cabf4198b7185464fc9b5f63dc257010a9cd
gianglaodai <gianghoang107@gmail.com>
2017-06-24 01:26:09 +0700

    cmt usermanager

--- base-prj/src/app/admin/admin.module.ts
+++ base-prj/src/app/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule,adminRoutedComponent } from './admin.routing';
 import {Angular2FontawesomeModule} from 'angular2-fontawesome/angular2-fontawesome';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserManagerComponent } from './user-manager/user-manager.component';
 
 @NgModule({
   imports: [AdminRoutingModule,Angular2FontawesomeModule],
   exports: [AdminRoutingModule],
-  declarations:[adminRoutedComponent, DashboardComponent]
+  declarations:[adminRoutedComponent, DashboardComponent, UserManagerComponent]
 })
 export class AdminModule { }
--- base-prj/src/app/admin/admin.routing.ts
+++ base-prj/src/app/admin/admin.routing.ts
@@ -3,20 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserManagerComponent } from './user-manager/user-manager.component';
 
 const routes: Routes = [
     {
         path: 'admin', component: HomeComponent, pathMatch: 'full',
         children: [
-             { path: '', redirectTo: './ dashboard', pathMatch: 'full' },
-            { path: 'dashboard', component: DashboardComponent }
+            { path: '', redirectTo: './dashboard', pathMatch: 'full' },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'usermanager', component: UserManagerComponent}
         ]
     }
 ]
-@NgModule({ 
+@NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
 export class AdminRoutingModule { }
 
-export const adminRoutedComponent = [SidebarComponent, HomeComponent, DashboardComponent];
+export const adminRoutedComponent = [SidebarComponent, HomeComponent, DashboardComponent,UserManagerComponent];
